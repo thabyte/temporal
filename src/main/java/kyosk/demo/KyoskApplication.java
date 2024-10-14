4004e3df9db218e95c31c2c73371628342fb2f31
Titus Muthomi <muthomititus+github@gmail.com>
2024-10-14 10:18:05 +0300

    Initial commit - set up app

---
+++ src/main/java/kyosk/demo/KyoskApplication.java
@@ -0,0 +1,13 @@
+package kyosk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyoskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyoskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kyosk/demo/KyoskApplicationTests.java
@@ -0,0 +1,13 @@
+package kyosk.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyoskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
